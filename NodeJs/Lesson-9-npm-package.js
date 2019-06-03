77660cefee604aee14e49acbfaf5ee67d9cd614e
vicToriOus <muzaffer.koluman@8bitiz.com>
2019-06-03 15:44:51 +0300

    global modullerini bagımlıksız gosterir

--- NodeJs/Lesson-9-npm-package.js
+++ NodeJs/Lesson-9-npm-package.js
@@ -3,4 +3,8 @@ const  slugify = require('slugify');
 const text = "Fenerbahçe üçüncü maçına çıkıyor"
 const slug = slugify(text,"+");
 
-console.log(slug)
+console.log(slug);
+
+//npm root -g global olan modullerin pwd'sini gosterir
+//npm list -g tüm global olan modullerin listesini bagımlılıklarıyla gosterir
+//npm list -g --depth=0 bagımlılıksız gosterir.
