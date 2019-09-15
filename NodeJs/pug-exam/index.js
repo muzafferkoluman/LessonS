31412b7b550031b3817cfddac36459a597bda68a
vicToriOus <muzaffer.koluman@8bitiz.com>
2019-09-15 16:12:49 +0300

    nodejs - express - pug - layout

--- NodeJs/pug-exam/index.js
+++ NodeJs/pug-exam/index.js
@@ -7,5 +7,11 @@ app.set('view engine','pug');
 app.get('/',(req,res)=>{
     res.render('index',{name:'muzaffer',surname:'koluman'});
 });
+app.get('/home',(req,res)=>{
+    res.render('home');
+});
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+});
 
 app.listen(3000);
