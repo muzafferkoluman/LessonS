ad6b05139572ab4d8938bed1277ca2e5b3bab0bb
vicToriOus <muzaffer.koluman@8bitiz.com>
2019-09-15 20:28:54 +0300

    nodejs - express - pug - static direction

---
+++ NodeJs/expressJs/static/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine','pug');
+app.use(express.static('public'));
+app.use(express.static('other-public'));
+
+app.get('/',(req,res)=>{
+    res.render('index')
+});
+
+app.listen(3000,()=>{
+    console.log('çalıştı.')
+});
