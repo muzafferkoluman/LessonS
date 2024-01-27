89befd977d7bebe6bf831cfbba84e95005e7cde3
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-01-27 03:24:21 +0100

    firebases data entered

---
+++ ReactNative/ehliyetSE/upload.js
@@ -0,0 +1,17 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+const data = require("./data.json");
+
+async function uploadData() {
+  for (const doc of data) {
+    await db.collection("questions").add(doc);
+  }
+}
+uploadData()
