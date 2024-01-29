57b4e22effce64212b84813b51b4e3064a847c7f
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-01-29 02:50:13 +0100

    some problem fixed

--- React/toggle-app/src/components/Collapse.js
+++ React/toggle-app/src/components/Collapse.js
@@ -1,26 +1,32 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 
-const Collapse = (props) => {
-  console.log(props);
-  return (
-    <div>
-        <a
-          className="btn btn-primary w-100"
-          data-toggle="collapse"
-          href={"#".concat(props.href)}
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
-          Link with href 
-        </a>
-        
-      <div className="collapse show" id={props.href}>
-            {props.children}
+class Collapse extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showContent: false,
+    };
+  }
+  showMore() {
+    this.setState ( {
+      showContent: true,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary w-100" onClick={this.showMore}>
+          Link with href
+        </button>
+
+        {this.state.showContent ? (
+          <div className="collapse show">{this.props.children}</div>
+        ) : null}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Collapse;
