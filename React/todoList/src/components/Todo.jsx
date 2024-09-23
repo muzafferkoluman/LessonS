1b033bf7e20c0d40a9fb1870fe80a6a17634f681
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-09-23 20:41:30 +0200

    input and button created and edited

--- React/todoList/src/components/Todo.jsx
+++ React/todoList/src/components/Todo.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { FaClipboardList } from "react-icons/fa";
+import { CiCirclePlus } from "react-icons/ci";
 
 const Todo = () => {
   return (
-    <div className="place-self-center bg-slate-100 w-[450px] h-[600px] p-12">
-      <h1 className="">todo test</h1>
+    <div className="place-self-center bg-slate-100 w-[450px] h-[600px] p-12 flex flex-col gap-5">
+      {/* Title */}
+      <h1 className="text-3xl font-bold tracking-wider flex items-center gap-3">
+        <FaClipboardList />
+        Todo App{" "}
+      </h1>
+
+      {/* Input and Search */}
+      <div className="flex items-center  bg-[#eee] rounded-full">
+        <input
+          type="text"
+          placeholder="you can create a new task"
+          className="border-none placeholder:text-slate-400 p-3.5 outline-none bg-transparent  flex-1 p-1"
+        />
+        <div className="bg-[#00ADB5] h-full w-14 flex items-center justify-center rounded-r-full">
+          <CiCirclePlus className="size-8 text-[#eee]" />
+        </div>
+      </div>
     </div>
   );
 };
