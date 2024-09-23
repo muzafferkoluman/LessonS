3dd9f562038e1b9dc575e2ae1e4c94d6e6347adf
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-09-23 20:48:49 +0200

    button edited again

--- React/todoList/src/components/Todo.jsx
+++ React/todoList/src/components/Todo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { FaClipboardList } from "react-icons/fa";
+import { FaClipboardList, FaPlus } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
 
 const Todo = () => {
   return (
-    <div className="place-self-center bg-slate-100 w-[450px] h-[600px] p-12 flex flex-col gap-5">
+    <div className="place-self-center bg-slate-100 w-[450px] h-[600px] p-12 flex flex-col gap-6">
       {/* Title */}
       <h1 className="text-3xl font-bold tracking-wider flex items-center gap-3">
         <FaClipboardList />
@@ -18,10 +18,12 @@ const Todo = () => {
           placeholder="you can create a new task"
           className="border-none placeholder:text-slate-400 p-3.5 outline-none bg-transparent  flex-1 p-1"
         />
-        <div className="bg-[#00ADB5] h-full w-14 flex items-center justify-center rounded-r-full">
-          <CiCirclePlus className="size-8 text-[#eee]" />
+        <div className="bg-[#00ADB5] h-full w-14 flex items-center justify-center rounded-r-full cursor-pointer">
+          <FaPlus className="size-5 text-[#eee]" />
         </div>
       </div>
+
+      {/*  */}
     </div>
   );
 };
