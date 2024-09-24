80c616509ae669372b41cf534cb15c607c8da5a2
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-09-24 22:16:48 +0200

    About useRef

--- React/todoList/src/components/Todo.jsx
+++ React/todoList/src/components/Todo.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FaClipboardList, FaPlus } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
 import Todoitem from "./Todoitem";
 
 const Todo = () => {
+    const [todos, setTodos] = useState([])
+    const data = useRef()
+
+    const addTodo =()=>{
+        console.log(data)
+    }
+
   return (
     <div className="place-self-center bg-slate-100 w-[450px] h-[600px] p-12 flex flex-col gap-8 rounded-xl">
       {/* Title */}
@@ -15,11 +22,12 @@ const Todo = () => {
       {/* Input and Search */}
       <div className="flex items-center  bg-[#eee] rounded-full">
         <input
+        ref={data}
           type="text"
           placeholder="you can create a new task"
           className="border-none placeholder:text-slate-400 p-3.5 outline-none bg-transparent  flex-1"
         />
-        <div className="bg-[#00ADB5] h-full w-14 flex items-center justify-center rounded-r-full cursor-pointer">
+        <div className="bg-[#00ADB5] h-full w-14 flex items-center justify-center rounded-r-full cursor-pointer" onClick={()=>addTodo()}>
           <FaPlus className="size-5 text-[#eee]" />
         </div>
       </div>
