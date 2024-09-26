5a41300c53e21bb1e2d6d8f94d1f441ff316e99d
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-09-26 23:45:24 +0200

    Title changed

--- React/todoList/src/components/Todo.jsx
+++ React/todoList/src/components/Todo.jsx
@@ -25,7 +25,7 @@ const Todo = () => {
 
   return (
     <div className="place-self-center bg-slate-100 w-[450px] h-[600px] p-12 flex flex-col gap-8 rounded-xl">
-      {/* Title */}
+      {/* Title Area*/}
       <h1 className="text-3xl font-bold tracking-wider flex items-center gap-3">
         <FaClipboardList />
         Todo App{" "}
