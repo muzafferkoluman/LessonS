0d83bfac0628d8c7b912ae944a49bd677bdd4902
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-09-30 17:43:58 +0200

    css added line-throug features

--- React/todoList/src/components/Todoitem.jsx
+++ React/todoList/src/components/Todoitem.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { FaRegCircle,FaRegTrashAlt,FaRegCalendar } from "react-icons/fa";
+import { FaRegCircle, FaRegTrashAlt, FaRegCheckCircle } from "react-icons/fa";
 
-const Todoitem = ({todo,toggle}) => {
+const Todoitem = ({ todo, toggle }) => {
   return (
-    <div className="flex items-center w-full py-4 px-2 gap-2 border-b  rounded-lg cursor-pointer" onClick={()=>toggle(todo.id)}>
-      {
-        todo.isComplete ? <FaRegCircle className="size-5 text-[#00ADB5]"/> : <FaRegCalendar className="size-5 text-[#1e4547]"/>
-      }
-      
-      <p className="flex-1">{todo.text}
-        </p>
-      <FaRegTrashAlt className="size-5 text-[#871231] hover:scale-110 transition-all"/>
+    <div
+      className="flex items-center w-full py-4 px-2 gap-2 border-b  rounded-lg cursor-pointer"
+      onClick={() => toggle(todo.id)}
+    >
+      {todo.isComplete ? (
+        <FaRegCheckCircle className="size-5 text-[#00ADB5]" />
+      ) : (
+        <FaRegCircle className="size-5 text-[#1e4547]" />
+      )}
+
+      <p
+        className={`flex-1 ${
+          todo.isComplete ? "line-through" : ""
+        }`}
+      >
+        {todo.text}
+      </p>
+      <FaRegTrashAlt className="size-5 text-[#871231] hover:scale-110 transition-all" />
     </div>
   );
 };
