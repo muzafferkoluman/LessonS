9f6378c73db4a622e8cb18170153bafafd0fdd75
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-09-30 16:58:59 +0200

    Added toggle effect to task

--- React/todoList/src/components/Todo.jsx
+++ React/todoList/src/components/Todo.jsx
@@ -13,7 +13,7 @@ const Todo = () => {
       return null;
     }
     console.log(inputText);
-    
+
     const newTodo = {
       id: todos.length + 1,
       text: inputText,
@@ -22,6 +22,16 @@ const Todo = () => {
     setTodos((prev) => [...prev, newTodo]);
     data.current.value = "";
   };
+  const toggle = (id) => {
+    setTodos((prevTodos) =>
+       prevTodos.map((todo) => {
+        if(todo.id ===id){
+          return {...todo, isComplete: !todo.isComplete}
+        }
+        return todo
+      }))
+    
+  }
 
   useEffect(() => {
     console.log(todos);
@@ -31,7 +41,7 @@ const Todo = () => {
     <div className="place-self-center bg-slate-100 w-[450px] h-[600px] p-12 flex flex-col gap-8 rounded-xl">
       {/* Title Area*/}
       <h1 className="text-3xl font-bold tracking-wider flex items-center gap-3">
-        <FaClipboardList />  
+        <FaClipboardList />
         Todo App{" "}
       </h1>
 
@@ -53,15 +63,9 @@ const Todo = () => {
 
       {/* Listing tasks */}
       <div className="mt-5">
-        {
-          todos.map((todo) => (
-            <Todoitem/>
-          ))
-        }
-        
-
-
-
+        {todos.map((todo) => (
+          <Todoitem key={todo.id} todo={todo} toggle={toggle} />
+        ))}
       </div>
     </div>
   );
--- React/todoList/src/components/Todoitem.jsx
+++ React/todoList/src/components/Todoitem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { FaRegCircle,FaRegTrashAlt } from "react-icons/fa";
 
-const Todoitem = () => {
+const Todoitem = ({todo,toggle}) => {
   return (
-    <div className="flex items-center w-full py-4 px-2 gap-2 border-b  rounded-lg cursor-pointer">
+    <div className="flex items-center w-full py-4 px-2 gap-2 border-b  rounded-lg cursor-pointer" onClick={()=>toggle(todo.id)}>
       <FaRegCircle className="size-5 text-[#00ADB5]"/>
-      <p className="flex-1">Test</p>
+      <p className="flex-1">{todo.text}</p>
       <FaRegTrashAlt className="size-5 text-[#871231] hover:scale-110 transition-all"/>
     </div>
   );
