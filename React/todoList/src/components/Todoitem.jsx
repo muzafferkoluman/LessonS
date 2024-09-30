5b8146e8e497112c01f54cc3cfb8d97210454c39
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-09-30 17:27:48 +0200

    toogle effect is actived

--- React/todoList/src/components/Todoitem.jsx
+++ React/todoList/src/components/Todoitem.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { FaRegCircle,FaRegTrashAlt } from "react-icons/fa";
+import { FaRegCircle,FaRegTrashAlt,FaRegCalendar } from "react-icons/fa";
 
 const Todoitem = ({todo,toggle}) => {
   return (
     <div className="flex items-center w-full py-4 px-2 gap-2 border-b  rounded-lg cursor-pointer" onClick={()=>toggle(todo.id)}>
-      <FaRegCircle className="size-5 text-[#00ADB5]"/>
-      <p className="flex-1">{todo.text}</p>
+      {
+        todo.isComplete ? <FaRegCircle className="size-5 text-[#00ADB5]"/> : <FaRegCalendar className="size-5 text-[#1e4547]"/>
+      }
+      
+      <p className="flex-1">{todo.text}
+        </p>
       <FaRegTrashAlt className="size-5 text-[#871231] hover:scale-110 transition-all"/>
     </div>
   );
