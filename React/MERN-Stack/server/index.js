1ac7077ccfeebb8ee853c0f93964f3f493148114
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-28 19:30:53 +0100

    server side's folders created

---
+++ React/MERN-Stack/server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
