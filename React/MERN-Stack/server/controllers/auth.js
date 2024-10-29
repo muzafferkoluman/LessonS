afb38eaaf93b98b4acebab47e031eb33de957b73
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-29 17:44:50 +0100

    controller pages added

---
+++ React/MERN-Stack/server/controllers/auth.js
@@ -0,0 +1,11 @@
+const register = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const login = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = { register, login };
