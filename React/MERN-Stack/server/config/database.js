7d2a582432ffd14cadd54ddd2d8584d60f779d0b
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-29 22:40:40 +0100

    jwt and bcrypt added on the project

--- React/MERN-Stack/server/config/database.js
+++ React/MERN-Stack/server/config/database.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const database = () => {
   mongoose
-    .connect("mongodb://localhost:27017/mernDB", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect("mongodb://localhost:27017/mernDB", 
+    // {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
+    )
     .then(() => {
       console.log("mongoDB connected");
     })
--- React/MERN-Stack/server/controllers/auth.js
+++ React/MERN-Stack/server/controllers/auth.js
@@ -1,5 +1,19 @@
+const AuthSchema = require("../models/auth");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
   try {
+    const { username, email, password } = req.body;
+    const user = await AuthSchema.findOne({ email });
+    if (user) {
+      return res.status(500).json({ message: "Email already exists" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(500)
+        .json({ message: "Password must be at least 6 characters" });
+    }
   } catch (error) {}
 };
 
