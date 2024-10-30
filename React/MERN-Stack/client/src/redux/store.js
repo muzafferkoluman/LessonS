d627f65ece13eeaf5a68bbd8a507474a8b91b84f
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-30 21:24:29 +0100

    Redux architecture installed

---
+++ React/MERN-Stack/client/src/redux/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const initialState = {};
+
+const reducers = combineReducers({
+
+});
+
+const store = createStore(reducers,initialState,composeWithDevTools(applyMiddleware(thunk)))
+
+export default store;
