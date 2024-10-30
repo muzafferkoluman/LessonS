fe2d5cbb6c0592250aea6bc8b60df6d23e75299c
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-30 23:46:01 +0100

    Auth page@2 fixed

--- React/MERN-Stack/client/src/pages/Auth.jsx
+++ React/MERN-Stack/client/src/pages/Auth.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
+  const [signUp, setSignUp] = useState(true);
   return (
     <div className="w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50">
-      <div className="w-1/3 bg-white p-3 ">
+      <div className="w-2/3 bg-white p-3 ">
         <h1 className="text-2xl text-gray-800 font-bold">Post Share</h1>
         <div className="flex flex-col space-y-3 my-5">
-          <input type="text" placeholder="Username" className="input-style" />
+          {signUp && <input type="text" placeholder="Username" className="input-style" />}
           <input type="text" placeholder="Email" className="input-style" />
-          <input type="password" placeholder="Password" className="input-style" />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input-style"
+          />
         </div>
-        <div className="text-red-500 text-sm cursor-pointer">Have you logged in before?</div>
+        <div className="text-red-500 text-xs cursor-pointer mb-4">
+            {signUp ? <span onClick={()=>setSignUp(false)}>Already have an account? Login</span> : <span onClick={()=>setSignUp(true)}>Don't have an account</span>}
+            
+        </div>
         <div className="cursor-pointer hover:bg-indigo-800 w-full p-2 text-center bg-indigo-500 text-gray-50">
-          Sign Up
+          {signUp ? "Sing Up" : "Register"}
         </div>
       </div>
     </div>
