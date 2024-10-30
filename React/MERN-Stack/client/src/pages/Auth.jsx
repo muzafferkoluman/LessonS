8de3d53d47b303df2e76f7c91b2217695a5c603b
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-31 00:01:39 +0100

    useState's default setting changed

--- React/MERN-Stack/client/src/pages/Auth.jsx
+++ React/MERN-Stack/client/src/pages/Auth.jsx
@@ -5,7 +5,7 @@ const Auth = () => {
   return (
     <div className="w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50">
       <div className="w-2/3 bg-white p-3 ">
-        <h1 className="text-2xl text-gray-800 font-bold">Post Share</h1>
+        <h1 className="text-2xl text-gray-800 font-bold">{signUp ? "Register" : "Login"}</h1>
         <div className="flex flex-col space-y-3 my-5">
           {signUp && <input type="text" placeholder="Username" className="input-style" />}
           <input type="text" placeholder="Email" className="input-style" />
@@ -17,7 +17,6 @@ const Auth = () => {
         </div>
         <div className="text-red-500 text-xs cursor-pointer mb-4">
             {signUp ? <span onClick={()=>setSignUp(false)}>Already have an account? Login</span> : <span onClick={()=>setSignUp(true)}>Don't have an account</span>}
-            
         </div>
         <div className="button-style">
           {signUp ? "Sing Up" : "Register"}
