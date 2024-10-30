9e9143dd2dc1d82c5aa91c28a6a7f5772b770823
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-30 22:26:32 +0100

    TailwindCss  architecture installed

--- React/MERN-Stack/client/src/App.js
+++ React/MERN-Stack/client/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home"
-import Auth from "./pages/Auth"
+import Auth from "./pages/Auth";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
+          <Route path="/auth" element={<Auth  />} />
         </Routes>
       </BrowserRouter>
     </div>
--- React/MERN-Stack/client/src/pages/Auth.jsx
+++ React/MERN-Stack/client/src/pages/Auth.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div className='w-full h-screen bg-gray-50 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50'>
+        <div className='w-1/3'>
+            Muzaffer
+        </div>
+    </div>
   )
 }
 
