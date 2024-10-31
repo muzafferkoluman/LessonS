7fbac80d62ee37732765bb0e59bdd60e3faf78ea
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-31 18:43:40 +0100

    text color changed

--- React/MERN-Stack/client/src/pages/Auth.jsx
+++ React/MERN-Stack/client/src/pages/Auth.jsx
@@ -5,7 +5,7 @@ const Auth = () => {
   return (
     <div className="w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50">
       <div className="w-2/3 bg-white p-3 ">
-        <h1 className="text-2xl text-gray-800 font-bold">{signUp ? "Register" : "Login"}</h1>
+        <h1 className="text-2xl text-blue-500 font-bold">{signUp ? "Register" : "Login"}</h1>
         <div className="flex flex-col space-y-3 my-5">
           {signUp && <input type="text" placeholder="Username" className="input-style" />}
           <input type="text" placeholder="Email" className="input-style" />
