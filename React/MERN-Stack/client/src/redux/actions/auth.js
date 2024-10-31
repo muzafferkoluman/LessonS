4db9649c5dabc0b9f108ddb283c78bd958f4aefe
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-10-31 21:29:34 +0100

    redux actions added

--- React/MERN-Stack/client/src/redux/actions/auth.js
+++ React/MERN-Stack/client/src/redux/actions/auth.js
@@ -1,1 +1,18 @@
  
+
+
+ export const registerAction =() => async(dispatch) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+ }
+
+ export const loginAction =() => async(dispatch) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+ }
--- React/MERN-Stack/client/src/redux/store.js
+++ React/MERN-Stack/client/src/redux/store.js
@@ -1,10 +1,13 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
+import authReducer from "./reducers/auth";
 
 const initialState = {};
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  auth: authReducer,
+});
 
 const store = createStore(
   reducers,
