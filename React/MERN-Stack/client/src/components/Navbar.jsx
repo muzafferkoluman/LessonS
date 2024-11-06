5ef809e80f167f0c9ac93bc271c7060631fdeef2
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-07 00:33:45 +0100

    login and logout bugs fixed

--- React/MERN-Stack/client/src/components/Navbar.jsx
+++ React/MERN-Stack/client/src/components/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import {BiLogOut} from "react-icons/bi"
+import { BiLogOut } from "react-icons/bi";
 
 const Navbar = () => {
+  const logoutFunc = ()=>{
+    localStorage.clear()
+    window.location.href = '/auth'
+  }
   return (
-    <div className="h-20 bg-indigo-400 flex items-center justify-between px-5"> 
-      <div className="text-white font-bold text-2xl cursor-pointer">POST SHARE</div>
-      <div>
-        <input type="text" placeholder="Search" />
-        <div>Create Post</div>
+    <div className="h-20 bg-indigo-600 flex items-center justify-between px-5">
+      <div className="text-white font-bold text-2xl cursor-pointer">
+        POST SHARE
+      </div>
+      <div className="flex items-center space-x-4">
+        <input
+          type="text"
+          placeholder="Search"
+          className="p-2 outline-none rounded-md"
+        />
+        <div className="text-white w-44 border p-2 text-center cursor-pointer hover:bg-indigo-700">
+          Create Post
+        </div>
+        <BiLogOut onClick={logoutFunc} size={25} className="text-white cursor-pointer"/>
       </div>
     </div>
   );
