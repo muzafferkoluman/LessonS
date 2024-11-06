3ba0a67d68eae99ac4eb1ebe4204072669c112e4
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-06 22:24:22 +0100

    We have token error but i should continue

--- React/MERN-Stack/client/src/App.js
+++ React/MERN-Stack/client/src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={!token.token ? <Link to={"/auth"}/>: <Home />} /> */}
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </BrowserRouter>
--- React/MERN-Stack/client/src/components/Navbar.jsx
+++ React/MERN-Stack/client/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import {BiLogOut} from "react-icons/bi"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="h-20 bg-indigo-400 flex items-center justify-between px-5"> 
+      <div className="text-white font-bold text-2xl cursor-pointer">POST SHARE</div>
+      <div>
+        <input type="text" placeholder="Search" />
+        <div>Create Post</div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
