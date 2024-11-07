05514ad18bf6533d8592625334f1f02831f476cb
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-07 22:03:34 +0100

    first init

---
+++ React/my-aws-todo-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const { task, setTask } = useState("");
+
+  return <div>
+    
+  </div>;
+}
+
+export default App;
