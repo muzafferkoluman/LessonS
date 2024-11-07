7ff8e7e77cda65532e147f02df6066866dee104f
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-08 00:52:02 +0100

    aws sns features added

--- React/my-aws-todo-app/src/App.js
+++ React/my-aws-todo-app/src/App.js
@@ -1,13 +1,36 @@
-import { useState,useRef } from "react";
-import "./App.css";
+import { useState, useRef } from "react";
+import Home from "./components/Home";
 
 function App() {
-  const { task, setTask } = useState("");
+  const [tasks, setTasks] = useState([]);
   const inputRef = useRef();
 
-  return <div>
+  const addTask = () => {
+    const task = inputRef.current.value;
+      setTasks([...tasks, task]);
+      inputRef.current.value = "";
+  };
 
-  </div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="New Task"
+            ref={inputRef} // useRef usage
+          />
+          <button
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+            onClick={addTask}
+          >
+            Add Task
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;
