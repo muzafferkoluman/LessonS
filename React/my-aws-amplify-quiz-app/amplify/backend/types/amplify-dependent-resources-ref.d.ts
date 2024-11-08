abb527faf75c15db90f035b418c2e89fbaa9f581
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-08 23:21:10 +0100

    amplify connection

---
+++ React/my-aws-amplify-quiz-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizapp9785fdd6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
