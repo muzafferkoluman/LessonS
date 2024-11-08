a0b9fea58820ea8338c5cb2504f519ed81dfb72f
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-08 16:46:04 +0100

    I have axios and lambda error

--- React/my-aws-lambda-app/src/App.js
+++ React/my-aws-lambda-app/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  const [taskName, setTaskName] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://9j7l0whl93.execute-api.eu-north-1.amazonaws.com/$default/tasks",
+        {
+          name: taskName,
+        }
+      );
+
+      console.log(response.data.message);
+    } catch (error) {
+      console.log("There was an error", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Task Name:
+          <input
+            type="text"
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add Task</button>
+      </form>
     </div>
   );
 }
