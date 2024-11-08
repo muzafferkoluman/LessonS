1c878a37af9fe536acf06235d11a45b010424648
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-09 00:04:14 +0100

    Quiz is actived

--- React/my-aws-amplify-quiz-app/src/App.js
+++ React/my-aws-amplify-quiz-app/src/App.js
@@ -16,7 +16,7 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 
 // Imports the Quiz component from Quiz.js for use in this file.
-//import Quiz from './Quiz';
+import Quiz from './Quiz';
 
 // Configures the Amplify library with the settings from aws-exports.js, which includes all the AWS service configurations for this project.
 Amplify.configure(awsExports);
@@ -29,7 +29,7 @@ function App() {
           <main>
             <header className='App-header'>
               {/* Quiz Component */}
-              {/* <Quiz /> */}
+              <Quiz /> 
               {/* Sign Out Button */}
               <button 
                 onClick={signOut} 
