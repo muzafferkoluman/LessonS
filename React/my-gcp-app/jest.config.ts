2b7da58f95e281ec71a89fa9b590b340fdc5ef68
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-11-19 17:54:37 +0100

    first init

---
+++ React/my-gcp-app/jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'my-gcp-app',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/my-gcp-app',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
