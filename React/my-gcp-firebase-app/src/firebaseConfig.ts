8c759eb5a107bd1c6bf0d93c898368629e95826e
Muzaffer Koluman <29776368+muzafferkoluman@users.noreply.github.com>
2024-12-01 03:10:53 +0100

    firebaseConfig page created

---
+++ React/my-gcp-firebase-app/src/firebaseConfig.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
